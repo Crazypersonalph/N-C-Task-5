e41083ba11c9c9842e2ded23bdd55c005ab7dbd1
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2024-08-29 11:31:54 +0800

    added a tiny tiny bit of commenting

--- yahtzo/utils/read_config.py
+++ yahtzo/utils/read_config.py
@@ -17,6 +17,6 @@ def read_config(config_file): # Define a function to read the configuration file
         num_dice = 3
         db_name = 'yahtzo.db'
         scoring = {'all-six': 100, 'all-of-kind': 70, 'straight': 50, 'full-house': 50, 'no-score': 0}
-        with open(config_file, 'w') as f:
+        with open(config_file, 'w') as f: # Create a new file containing the default configuration
             json.dump({'num_dice': num_dice, 'db_name': db_name, 'scoring': scoring}, f, indent=4)
         return Config(num_dice, db_name, scoring, None) # Return the default configuration if the file doesn't exist or is invalid
